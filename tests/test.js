c13cf740b85651e9da0699b46a708f722b9cd4be
tin.nguyen <tin.nguyen@rakuten.com>
2023-04-23 15:25:08 +0900

    Not just processing and returning the images, we need a storage for those - Postgresql

--- tests/test.js
+++ tests/test.js
@@ -1,28 +1,42 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { sleep, check } from 'k6';
+
+const binFile = open('test_img.jpeg', 'b');
 
 export let options = {
-  vus: 1,
-  duration: '10s',
+  stages: [
+    { duration: '30s', target: 10 },
+    { duration: '30s', target: 0 },
+  ],
+  thresholds: {
+    http_req_duration: ['p(99)<500'],
+    http_req_failed: ['rate<0.1']
+  },
 };
 
 export default function () {
-  const url = 'http://localhost:4000/api/v1/load';
-  const imgFile = open('./image.jpg', 'b');
-  const imgData = imgFile.readAll();
-
-  const params = {
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
-    tags: { name: 'test-upload' },
-    data: {
-      file: http.file(imgData, 'image.jpg'),
-    },
+  const data = {
+    field: 'this is a standard form field',
+    file: http.file(binFile, 'image.jpg'),
   };
 
-  http.post(url, params);
-  imgFile.close();
+  let res = http.batch([
+    { method: 'POST', url: 'http://localhost:4000/api/v1/upload', body: data },
+    { method: 'POST', url: 'http://localhost:4000/api/v1/upload', body: data },
+    { method: 'POST', url: 'http://localhost:4000/api/v1/upload', body: data },
+    { method: 'POST', url: 'http://localhost:4000/api/v1/upload', body: data },
+    { method: 'POST', url: 'http://localhost:4000/api/v1/upload', body: data },
+    { method: 'POST', url: 'http://localhost:4000/api/v1/upload', body: data },
+    { method: 'POST', url: 'http://localhost:4000/api/v1/upload', body: data },
+    { method: 'POST', url: 'http://localhost:4000/api/v1/upload', body: data },
+    { method: 'POST', url: 'http://localhost:4000/api/v1/upload', body: data },
+    { method: 'POST', url: 'http://localhost:4000/api/v1/upload', body: data },
+  ]);
+
+  check(res[0], {
+    'status is 200': (r) => r.status === 200,
+    'response body': (r) => r.body.indexOf('success') !== -1,
+  });
 
   sleep(1);
 }
